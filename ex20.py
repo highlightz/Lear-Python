f607494608ece1b6161d538dc6216db06b33dac2
Highlight Clark <highlightz@163.com>
2015-08-22 22:55:33 +0800

    Create ex20.py

---
+++ ex20.py
@@ -0,0 +1,43 @@
+from sys import argv
+
+script, input_file = argv
+
+def print_all( f ):
+	print f.read( )
+
+# What does this function do?
+def rewind( f ):
+	f.seek( 0 )
+	
+def print_a_line( line_count, f ):
+	print line_count, f.readline( )
+	
+current_file = open( input_file )
+
+print "First let's print the whole file:\n"
+
+print_all( current_file )
+
+print "Now let's rewind, kind of like a tape."
+
+rewind( current_file )
+
+print "Let's print three lines:"
+
+current_line = 1
+print_a_line( current_line, current_file )
+
+current_line += 1
+print_a_line( current_line, current_file )
+
+current_line += 1
+print_a_line( current_line, current_file )
+
+# Why does readline function always work right?
+"""
+In fact, when openning a file with open in python shell environment,
+readline function can read a line at a time, as if it can maintain 
+some state info.
+
+File object in python is responsible for state maintainance.
+"""
